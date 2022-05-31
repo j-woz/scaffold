dc8a88964045903155a6c7cfd21ad74b8ab53cda
Justin Wozniak <woz@anl.gov>
2022-05-31 15:24:54 +0200

    Sketch call to decide()

--- search_matcher.swift
+++ search_matcher.swift
@@ -4,7 +4,7 @@
   o = python_persist(
 ----
 from search_matcher import SearchMatcher
-target = '%s'
+target = "%s"
 start  =  %i
 length =  %i
 L = []  # List of strings
@@ -12,5 +12,16 @@ for i in range(0, start):
     L.append(str(SearchMatcher(target, None, None, length)))
 result = ";".join(L)
 ---- % (target, start, length),
+"result");
+}
+
+(string o) decide(string target, string candidate)
+{
+  o = python_persist(
+----
+target    = "%s"
+candidate = "%s"
+result = "x;y;z;" + target + candidate
+---- % (target, candidate),
 "result");
 }
--- workflow.swift
+++ workflow.swift
@@ -8,10 +8,17 @@ import search_matcher;
 start_count  = 10;
 start_length = 5;
 target = "justin";
+max_levels = 3;
 
-s = start_random(target, start_count, start_length);
-string items[] = split(s, ";");
+s0 = start_random(target, start_count, start_length);
+string items[] = split(s0, ";");
 foreach item in items
 {
   printf(item);
+}
+
+foreach item in items
+{
+  s1 = decide(target, item);
+  printf(s1);
 }
